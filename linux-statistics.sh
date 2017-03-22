a5b2b4a4f037e169c3058b7e2c75d6a6bd7e23c6
Yrjö Kari-Koskinen <ykk@peruna.fi>
2017-03-22 23:01:52 +0200

    sort tags numerically

--- linux-statistics.sh
+++ linux-statistics.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-TAGS=`git tag|grep -v -- -|grep -vF 2.6.11`
+TAGS=`git tag|grep -v -- -|grep -vF 2.6.11|cut -b 2-|sort -t. -k1,1n -k2,2n`
 
 for tag in $TAGS; do
-    git checkout $tag >/dev/null 2>&1
+    git checkout v$tag >/dev/null 2>&1
     numfiles=`find . -type f|wc -l`
     treesize=`du -scm *|tail -1|cut -f1`
     echo $tag $treesize $numfiles
